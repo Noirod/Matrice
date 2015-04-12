249f839caafa1d66a0c4809939fadff69040a9b3
Thomas <Jacumin.Thomas@live.fr>
2015-04-12 16:03:14 +0200

    Ajout des fichiers de base

---
+++ CMakeFiles/matrice.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrice.dir/src/main.o"
+  "matrice.pdb"
+  "matrice"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matrice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
